f3e652390d5601ca065b96f858a46fe253440bbb
vokod <vokoda@gmail.com>
2018-01-10 13:52:29 +0100

    Changed feature according to Tibi's review. Main changes:  - DonationPoint     - created parameterized constructor, refactored class to be immutable  - DummyRepository     - added method to provide single DonationPoint  - MapActivity     - FAB animates in and out in sync with the selection state of a marker on map     - In both singlepoint and multiplepoint mode the donation point(s) are in a list     - Selected donation point is accessed via a list index     - Activity maintains selection state on config. changes     - MapView maintains position and zoom state on config. changes     - Method names are less confusing :)

--- app/src/main/java/hu/intellicode/savethreelivestoday/ui/map/DonationPoint.java
+++ app/src/main/java/hu/intellicode/savethreelivestoday/ui/map/DonationPoint.java
@@ -5,54 +5,39 @@ import com.google.android.gms.maps.model.LatLng;
 import java.io.Serializable;
 
 /**
- * Egyszerű POJO a MapActivity-nek szükséges adatokkal
+ * Simple POJO with the data required by MapActivity
  */
 public class DonationPoint implements Serializable {
 
-    private String name;
-    private String address;
-    private double latitude;
-    private double longitude;
-    // esetleg egyéb adatok: szükséges vércsoport, nyitvatartás, satöbbi
+    private final String name;
+    private final String address;
+    private final double latitude;
+    private final double longitude;
+
+    public DonationPoint(String name, String address, double latitude, double longitude) {
+        this.name = name;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     String getAddress() {
         return address;
     }
 
-    void setAddress(String address) {
-        this.address = address;
-    }
-
     double getLatitude() {
         return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
     }
 
     double getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    void setCoordinates(double latitude, double longitude){
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    LatLng getCoordinates(){
-        return new LatLng(latitude,longitude);
+    LatLng getCoordinates() {
+        return new LatLng(latitude, longitude);
     }
 }
--- app/src/main/java/hu/intellicode/savethreelivestoday/ui/map/MapActivity.java
+++ app/src/main/java/hu/intellicode/savethreelivestoday/ui/map/MapActivity.java
@@ -14,32 +14,42 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import hu.intellicode.savethreelivestoday.R;
 import hu.intellicode.savethreelivestoday.ui.BaseActivity;
 
-// TODO: map API restriction-öket beállítani (kulcsok kellenek hozzá debug és release buildekhez)
+// TODO: setup map API restrictions in Google Play Console for the GoogleMaps API key. (needs release and debug keys)
 public class MapActivity extends BaseActivity implements
         OnMapReadyCallback,
-        GoogleMap.OnMarkerClickListener {
+        GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnMapClickListener {
 
-    private static final String EXTRA_DONATION_POINT = "extra_donation_point";
-    public static final String KEY_DONATION_POINT = "key_donation_point";
-    public static final String KEY_FIRST_RUN = "key_first_run";
-    private static final String TAG = MapActivity.class.getSimpleName();
-    private final static int CAMERA_ZOOM_LEVEL = 15;
+    private final static String KEY_DONATION_POINT = "key_donation_point";
+    private final static String KEY_FIRST_RUN = "key_first_run";
+    private final static String KEY_SELECTED_DONATION_POINT_INDEX = "key_selected_donation_point_index";
+    private final static String KEY_MAP_CAMERA_POSITION = "key_map_screen_location";
+    private final static String EXTRA_DONATION_POINT = "extra_donation_point";
+    private final static String TAG = MapActivity.class.getSimpleName();
+    private final static int INITIAL_CAMERA_ZOOM_LEVEL = 15;
+    private final static int INITIAL_MAP_PADDING = 100;
+    private final static int FAB_SLIDE_DISTANCE = 400;
 
     private MapView mapView;
     private GoogleMap googleMap;
-    private DonationPoint donationPoint;
+    private int indexOfSelectedDonationPoint;
+    private boolean singlePointMode;
     private List<DonationPoint> donationPoints;
+    private FloatingActionButton fab;
     private boolean firstRun = true;
+    private CameraPosition savedCameraPosition;
 
     public static Intent getStarterIntent(Context context, DonationPoint donationPoint) {
         Intent intent = new Intent(context, MapActivity.class);
@@ -55,48 +65,64 @@ public class MapActivity extends BaseActivity implements
 
         View view = getLayoutInflater().inflate(R.layout.activity_map, contentContainer, false);
         Toolbar toolbar = view.findViewById(R.id.toolbar);
-
-        mapView = view.findViewById(R.id.map_view);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
-
-        if (getIntent().hasExtra(EXTRA_DONATION_POINT)) {
-            Log.d(TAG, "Donation point set from intent");
-            this.donationPoint = (DonationPoint) getIntent().getSerializableExtra(EXTRA_DONATION_POINT);
-        }
-
-        if (savedInstanceState != null) {
-            if (savedInstanceState.containsKey(KEY_DONATION_POINT)) {
-                this.firstRun = savedInstanceState.getBoolean(KEY_FIRST_RUN);
-            }
-            if (donationPoint == null && savedInstanceState.containsKey(KEY_DONATION_POINT)) {
-                this.donationPoint = (DonationPoint) savedInstanceState.getSerializable(KEY_DONATION_POINT);
-                Log.d(TAG, "Donation point set from savedInstanceState");
-            }
-        }
-        if (donationPoint == null) {
-            Log.d(TAG, "No donation point - must fetch points from db/api");
-            this.donationPoints = DummyRepository.getAllDonationPoints();
-        }
-
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         contentContainer.addView(view);
 
-        FloatingActionButton fab = view.findViewById(R.id.fab_directions);
+        mapView = view.findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        if (savedInstanceState != null) {
+            // activity restarted for whatever reason
+            this.firstRun = savedInstanceState.getBoolean(KEY_FIRST_RUN);
+            this.savedCameraPosition = savedInstanceState.getParcelable(KEY_MAP_CAMERA_POSITION);
+            if (savedInstanceState.containsKey(KEY_DONATION_POINT)) {
+                singlePointMode = true;
+                donationPoints = new ArrayList<>(1);
+                donationPoints.add((DonationPoint) savedInstanceState.getSerializable(KEY_DONATION_POINT));
+                Log.d(TAG, "Donation point set from savedInstanceState");
+            }
+            if (savedInstanceState.containsKey(KEY_SELECTED_DONATION_POINT_INDEX)) {
+                indexOfSelectedDonationPoint =
+                        savedInstanceState.getInt(KEY_SELECTED_DONATION_POINT_INDEX);
+            } else {
+                indexOfSelectedDonationPoint = -1;
+            }
+        } else {
+            if (getIntent().hasExtra(EXTRA_DONATION_POINT)) {
+                // activity started for the first time in single point mode
+                singlePointMode = true;
+                donationPoints = new ArrayList<>(1);
+                donationPoints.add((DonationPoint) getIntent().getSerializableExtra(EXTRA_DONATION_POINT));
+                indexOfSelectedDonationPoint = 0;
+                //selectedDonationPoint = donationPoints.get(0);
+                Log.d(TAG, "Donation point set from intent");
+            } else {
+                // activity started for the first time in multiple point mode
+                indexOfSelectedDonationPoint = -1;
+            }
+        }
+        if (donationPoints == null) {
+            Log.d(TAG, "No donation point - must fetch points from db/api");
+            this.donationPoints = DummyRepository.getAllDonationPoints();
+        }
+
+        fab = view.findViewById(R.id.fab_directions);
+        if (indexOfSelectedDonationPoint < 0) {
+            hideOutFab();
+        }
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(donationPoint!=null) {
-                    Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
-                            Uri.parse("http://maps.google.com/maps?daddr="
-                                    + String.valueOf(donationPoint.getLatitude())
-                                    + ","
-                                    + String.valueOf(donationPoint.getLongitude())));
-                    startActivity(intent);
-                }
+                Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("http://maps.google.com/maps?daddr="
+                                + String.valueOf(donationPoints.get(indexOfSelectedDonationPoint).getLatitude())
+                                + ","
+                                + String.valueOf(donationPoints.get(indexOfSelectedDonationPoint).getLongitude())));
+                startActivity(intent);
             }
         });
     }
@@ -118,62 +144,91 @@ public class MapActivity extends BaseActivity implements
         Log.d(TAG, "onMapReady");
 
         googleMap.setOnMarkerClickListener(this);
+        googleMap.setOnMapClickListener(this);
+        googleMap.getUiSettings().setMapToolbarEnabled(false);
 
         this.googleMap = googleMap;
-        if (donationPoints != null) {
-            setMarkers(firstRun);
+        if (singlePointMode) {
+            if (indexOfSelectedDonationPoint >= 0) {
+                addSingleMarkerToMap(donationPoints.get(0), firstRun, true);
+            } else {
+                addSingleMarkerToMap(donationPoints.get(0), firstRun, false);
+            }
         } else {
-            setMarker(donationPoint, firstRun);
+            addMultipleMarkersToMap(donationPoints, firstRun);
         }
         firstRun = false;
     }
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-        donationPoint = new DonationPoint();
-        donationPoint.setName(marker.getTitle());
-        donationPoint.setAddress(marker.getSnippet());
-        donationPoint.setCoordinates(marker.getPosition().latitude, marker.getPosition().longitude);
+        indexOfSelectedDonationPoint = (int) marker.getTag();
+        slideInFab();
         return false;
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        indexOfSelectedDonationPoint = -1;
+        slideOutFab();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putSerializable(KEY_DONATION_POINT, donationPoint);
         outState.putBoolean(KEY_FIRST_RUN, firstRun);
+        outState.putInt(KEY_SELECTED_DONATION_POINT_INDEX, indexOfSelectedDonationPoint);
+        outState.putParcelable(KEY_MAP_CAMERA_POSITION, googleMap.getCameraPosition());
+        if (donationPoints.size() == 1) {
+            outState.putSerializable(KEY_DONATION_POINT, donationPoints.get(0));
+        }
     }
 
-    private void setMarkers(boolean firstRun) {
+    private void addSingleMarkerToMap(DonationPoint donationPoint, boolean firstRun, boolean selected) {
+        Marker marker = setMarker(donationPoint, 0, selected);
+        if (firstRun) {
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), INITIAL_CAMERA_ZOOM_LEVEL));
+        } else {
+            googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(savedCameraPosition));
+        }
+    }
+
+    private void addMultipleMarkersToMap(List<DonationPoint> donationPoints, boolean firstRun) {
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
-        for (DonationPoint p : donationPoints) {
-            builder.include(setMarker(p).getPosition());
+        for (int i = 0; i < donationPoints.size(); i++) {
+            if (i == indexOfSelectedDonationPoint) {
+                builder.include(setMarker(donationPoints.get(i), i, true).getPosition());
+            } else {
+                builder.include(setMarker(donationPoints.get(i), i, false).getPosition());
+            }
         }
         if (firstRun) {
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 100));
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), INITIAL_MAP_PADDING));
         } else {
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 100));
+            googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(savedCameraPosition));
         }
     }
 
-    private void setMarker(DonationPoint donationPoint, boolean firstRun) {
-        Marker marker = setMarker(donationPoint);
-        marker.showInfoWindow();
-        if (firstRun) {
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), CAMERA_ZOOM_LEVEL));
-        } else {
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), CAMERA_ZOOM_LEVEL));
-        }
-    }
-
-    private Marker setMarker(DonationPoint donationPoint) {
+    private Marker setMarker(DonationPoint donationPoint, int tag, boolean isSelected) {
         Marker marker = googleMap
                 .addMarker(new MarkerOptions()
                         .position(donationPoint.getCoordinates())
                         .snippet(donationPoint.getAddress())
                         .title(donationPoint.getName()));
+        marker.setTag(tag);
+        if (isSelected) marker.showInfoWindow();
         return marker;
     }
 
+    private void slideInFab() {
+        fab.animate().translationX(0);
+    }
 
+    private void slideOutFab() {
+        fab.animate().translationX(FAB_SLIDE_DISTANCE);
+    }
+
+    private void hideOutFab() {
+        fab.setX(fab.getX() + FAB_SLIDE_DISTANCE);
+    }
 }
